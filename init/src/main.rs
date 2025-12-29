9d3a1c6c35063ef4351a759f446a236328614bbe
rtthw <172864013+rtthw@users.noreply.github.com>
2025-12-29 09:09:19 -0500

    Improve terminal output during init sequence

--- init/src/main.rs
+++ init/src/main.rs
@@ -1,7 +1,5 @@
 //! # Init System
 
-use std::collections::HashMap;
-
 use kernel::{
     Result,
     c_str::NULL_CSTR,
@@ -19,22 +17,32 @@ fn main() {
         exit(-1)
     }
 
-    println!("init: Mounting filesystems...");
+    println!("\x1b[2minit\x1b[0m: Mounting filesystems...");
 
     if let Err(error) = setup_mount_points() {
-        println!("ERROR (init): Failed to mount filesystems: {error}");
+        println!("\x1b[33mERROR\x1b[0m \x1b[2m(init)\x1b[0m: Failed to mount filesystems: {error}");
         exit(-1)
     }
 
     // Make sure this process is a session leader.
     _ = setsid();
 
-    println!("init: Blocking signals...");
+    println!("\x1b[2minit\x1b[0m: Blocking signals...");
 
     let mask = SignalMask::all();
     mask.block().unwrap();
 
-    println!("init: Starting main loop...");
+    println!("\x1b[2minit\x1b[0m: Scanning filesystem...");
+
+    if let Err(error) = print_filesystem() {
+        println!(
+            "\n\x1b[33mERROR\x1b[0m \x1b[2m(init)\x1b[0m: Failed to read filesystem: {error}\n",
+        );
+    } else {
+        println!();
+    }
+
+    println!("\x1b[2minit\x1b[0m: Starting main loop...");
 
     loop {
         if let Ok(sig) = mask.wait() {
@@ -57,6 +65,40 @@ fn setup_mount_points() -> Result<()> {
     // mount(c"tmpfs", c"/dev/shm", c"tmpfs",    NOSUID | NOEXEC | NODEV, Some(c"mode=1777"))?;
 
     Ok(())
+}
+
+fn print_filesystem() -> Result<()> {
+    const IGNORE: &[&str] = &[
+        "/proc",
+        "/sys/class",
+        "/sys/kernel/slab",
+        "/sys/devices",
+    ];
+
+    fn inner(dir: &str, depth: usize) -> Result<()> {
+        for entry in std::fs::read_dir(dir).map_err(|_| kernel::Error::NOENT)? {
+            let entry = entry.map_err(|_| kernel::Error::NOENT)?;
+            let path = entry.path().to_str().unwrap().to_string();
+            let name = path.rsplit_once('/').map_or(path.clone(), |(_, name)| name.to_string());
+
+            println!("{}\x1b[2m/\x1b[0m{}", "    ".repeat(depth), name);
+
+            if entry.file_type().map_err(|_| kernel::Error::BADF)?.is_dir() {
+                if IGNORE.contains(&path.as_str()) {
+                    println!("{}\x1b[2m/...\x1b[0m", "    ".repeat(depth + 1));
+                    return Ok(());
+                }
+
+                inner(&path, depth + 1)?;
+            }
+        }
+
+        Ok(())
+    }
+
+    println!("\x1b[2m/\x1b[0m");
+
+    inner("/", 1)
 }
 
 fn handle_sigchld() {
