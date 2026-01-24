324ab43b3a3d56ee57325a788c8a25f8d5ed617d
rtthw <172864013+rtthw@users.noreply.github.com>
2026-01-24 06:07:46 -0500

    Fix invalid relative path for ABI tests

--- qemu.sh
+++ qemu.sh
@@ -32,7 +32,7 @@ cd initrd
 
     # Put the ABI where the compiler can find it.
     cp ../../target/x86_64-unknown-linux-gnu/release/libabi.rlib ../rootfs/lib/
-    cp ../../abi-tests/src/abi_tests.rs ../rootfs/lib/
+    cp ../../crates/abi-tests/src/abi_tests.rs ../rootfs/lib/
 
     # Put the example program where the compiler can find it.
     cp ../../example/src/example.rs ../rootfs/lib/
