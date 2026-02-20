c902723f50b8c5bc2d4a679f2ea24628c9cf52f9
rtthw <172864013+rtthw@users.noreply.github.com>
2026-02-20 09:28:30 -0500

    Flesh out more of the ABI's ELF parser

--- crates/abi/src/elf.rs
+++ crates/abi/src/elf.rs
@@ -21,6 +21,35 @@ impl<'a> ElfFile<'a> {
         Ok(Self { input, header })
     }
 
+    pub fn section_iter(&self) -> impl Iterator<Item = &SectionHeader> + '_ {
+        SectionIter {
+            file: self,
+            next_index: 0,
+        }
+    }
+
+    pub fn find_section_by_name(&self, name: &str) -> Option<&SectionHeader> {
+        for sect in self.section_iter() {
+            if let Ok(sect_name) = sect.get_name(self) {
+                if sect_name == name {
+                    return Some(sect);
+                }
+            }
+        }
+
+        None
+    }
+
+    pub fn get_string(&self, index: u32) -> Result<&'a str, &'static str> {
+        let header = self
+            .find_section_by_name(".strtab")
+            .ok_or("no `.strtab` section")?;
+        if header.get_type()? != SectionHeaderType::StrTab {
+            return Err("expected `.strtab` to be a string table");
+        }
+        Ok(read_str(&header.raw_data(self)[(index as usize)..]))
+    }
+
     pub fn get_section_header(&self, index: u16) -> Result<&'a SectionHeader, &'static str> {
         SectionHeader::parse(self.input, self.header, index)
     }
@@ -40,6 +69,19 @@ impl<'a> ElfFile<'a> {
         self.get_shstr_table().map(|shstr_table| unsafe {
             str::from_utf8_unchecked(read_until_null(&shstr_table[(index as usize)..]))
         })
+    }
+
+    pub fn get_symbol_table(&self) -> Result<&[SymbolTableEntry], &'static str> {
+        let symtab_data = self
+            .section_iter()
+            .find(|sec| sec.get_type() == Ok(SectionHeaderType::SymTab))
+            .ok_or("no `symtab` section")
+            .and_then(|s| s.get_data(self));
+
+        match symtab_data {
+            Ok(SectionData::SymbolTable(symtab)) => Ok(symtab),
+            _ => Err("no symbol table found, file may have been stripped"),
+        }
     }
 }
 
@@ -78,6 +120,17 @@ impl<'a> Header<'a> {
             unsafe { pod_read(&bytes[HEADER_IDENT_SIZE..HEADER_IDENT_SIZE + HEADER_BODY_SIZE]) };
 
         Ok(Header { ident, body })
+    }
+
+    pub fn get_type(&self) -> ObjectFileType {
+        match self.body.type_ {
+            0 => ObjectFileType::None,
+            1 => ObjectFileType::Relocatable,
+            2 => ObjectFileType::Executable,
+            3 => ObjectFileType::SharedObject,
+            4 => ObjectFileType::Core,
+            x => ObjectFileType::ProcessorSpecific(x),
+        }
     }
 }
 
@@ -158,6 +211,15 @@ impl fmt::Display for HeaderBody {
     }
 }
 
+#[derive(Clone, Copy, Debug, Eq, PartialEq)]
+pub enum ObjectFileType {
+    None,
+    Relocatable,
+    Executable,
+    SharedObject,
+    Core,
+    ProcessorSpecific(u16),
+}
 
 
 pub const SHN_UNDEF: u16 = 0;
@@ -208,11 +270,170 @@ impl SectionHeader {
     }
 
     pub fn get_name<'a>(&self, file: &ElfFile<'a>) -> Result<&'a str, &'static str> {
-        if self.type_ == 0 {
+        if self.get_type()? == SectionHeaderType::Null {
             return Err("Attempted to get name of null section");
         }
         file.get_shstr(self.name)
     }
+
+    pub fn get_type(&self) -> Result<SectionHeaderType, &'static str> {
+        match self.type_ {
+            0 => Ok(SectionHeaderType::Null),
+            1 => Ok(SectionHeaderType::ProgBits),
+            2 => Ok(SectionHeaderType::SymTab),
+            3 => Ok(SectionHeaderType::StrTab),
+            4 => Ok(SectionHeaderType::Rela),
+            5 => Ok(SectionHeaderType::Hash),
+            6 => Ok(SectionHeaderType::Dynamic),
+            7 => Ok(SectionHeaderType::Note),
+            8 => Ok(SectionHeaderType::NoBits),
+            9 => Ok(SectionHeaderType::Rel),
+            10 => Ok(SectionHeaderType::ShLib),
+            11 => Ok(SectionHeaderType::DynSym),
+            // sic.
+            14 => Ok(SectionHeaderType::InitArray),
+            15 => Ok(SectionHeaderType::FiniArray),
+            16 => Ok(SectionHeaderType::PreInitArray),
+            17 => Ok(SectionHeaderType::Group),
+            18 => Ok(SectionHeaderType::SymTabShIndex),
+            n if (SHT_LOOS..=SHT_HIOS).contains(&n) => Ok(SectionHeaderType::OsSpecific(n)),
+            n if (SHT_LOPROC..=SHT_HIPROC).contains(&n) => {
+                Ok(SectionHeaderType::ProcessorSpecific(n))
+            }
+            n if (SHT_LOUSER..=SHT_HIUSER).contains(&n) => Ok(SectionHeaderType::User(n)),
+            _ => Err("Invalid section header type"),
+        }
+    }
+
+    pub fn get_data<'a>(&self, file: &ElfFile<'a>) -> Result<SectionData<'a>, &'static str> {
+        self.get_type().and_then(|typ| {
+            Ok(match typ {
+                SectionHeaderType::Null | SectionHeaderType::NoBits => SectionData::Empty,
+                SectionHeaderType::ProgBits
+                | SectionHeaderType::ShLib
+                | SectionHeaderType::OsSpecific(_)
+                | SectionHeaderType::ProcessorSpecific(_)
+                | SectionHeaderType::User(_) => SectionData::Undefined(self.raw_data(file)),
+                SectionHeaderType::SymTab => {
+                    let data = self.raw_data(file);
+                    SectionData::SymbolTable(read_array(data))
+                }
+                SectionHeaderType::DynSym => {
+                    let data = self.raw_data(file);
+                    SectionData::DynSymbolTable(read_array(data))
+                }
+                SectionHeaderType::StrTab => SectionData::StrArray(self.raw_data(file)),
+                SectionHeaderType::InitArray
+                | SectionHeaderType::FiniArray
+                | SectionHeaderType::PreInitArray => {
+                    let data = self.raw_data(file);
+                    SectionData::FnArray(read_array(data))
+                }
+                SectionHeaderType::Rela => {
+                    let data = self.raw_data(file);
+                    SectionData::Rela(read_array(data))
+                }
+                SectionHeaderType::Rel => {
+                    let data = self.raw_data(file);
+                    SectionData::Rel(read_array(data))
+                }
+                SectionHeaderType::Dynamic => {
+                    todo!()
+                    // let data = self.raw_data(file);
+                    // SectionData::Dynamic(read_array(data))
+                }
+                SectionHeaderType::Group => {
+                    let data = self.raw_data(file);
+                    unsafe {
+                        let flags: &'a u32 = std::mem::transmute(&data[0]);
+                        let indices: &'a [u32] = read_array(&data[4..]);
+                        SectionData::Group { flags, indices }
+                    }
+                }
+                SectionHeaderType::SymTabShIndex => {
+                    SectionData::SymTabShIndex(read_array(self.raw_data(file)))
+                }
+                SectionHeaderType::Note => todo!(),
+                SectionHeaderType::Hash => todo!(),
+            })
+        })
+    }
+
+    pub fn raw_data<'a>(&self, file: &ElfFile<'a>) -> &'a [u8] {
+        assert_ne!(self.get_type().unwrap(), SectionHeaderType::Null);
+        &file.input[self.offset() as usize..(self.offset() + self.size()) as usize]
+    }
+
+    #[inline]
+    pub const fn address(&self) -> u64 {
+        self.address
+    }
+
+    #[inline]
+    pub const fn size(&self) -> u64 {
+        self.size
+    }
+
+    #[inline]
+    pub const fn align(&self) -> u64 {
+        self.align
+    }
+
+    #[inline]
+    pub const fn offset(&self) -> u64 {
+        self.offset
+    }
+
+    #[inline]
+    pub const fn flags(&self) -> u64 {
+        self.flags
+    }
+
+    #[inline]
+    pub const fn name(&self) -> u32 {
+        self.name
+    }
+
+    #[inline]
+    pub const fn link(&self) -> u32 {
+        self.link
+    }
+
+    #[inline]
+    pub const fn info(&self) -> u32 {
+        self.info
+    }
+}
+
+pub const SHT_LOOS: u32 = 0x60000000;
+pub const SHT_HIOS: u32 = 0x6fffffff;
+pub const SHT_LOPROC: u32 = 0x70000000;
+pub const SHT_HIPROC: u32 = 0x7fffffff;
+pub const SHT_LOUSER: u32 = 0x80000000;
+pub const SHT_HIUSER: u32 = 0xffffffff;
+
+#[derive(Clone, Copy, Debug, Eq, PartialEq)]
+pub enum SectionHeaderType {
+    Null,
+    ProgBits,
+    SymTab,
+    StrTab,
+    Rela,
+    Hash,
+    Dynamic,
+    Note,
+    NoBits,
+    Rel,
+    ShLib,
+    DynSym,
+    InitArray,
+    FiniArray,
+    PreInitArray,
+    Group,
+    SymTabShIndex,
+    OsSpecific(u32),
+    ProcessorSpecific(u32),
+    User(u32),
 }
 
 pub struct SectionIter<'input, 'file> {
@@ -235,6 +456,120 @@ impl<'input, 'file> Iterator for SectionIter<'input, 'file> {
     }
 }
 
+#[derive(Debug)]
+pub enum SectionData<'a> {
+    Empty,
+    Undefined(&'a [u8]),
+    Group { flags: &'a u32, indices: &'a [u32] },
+    StrArray(&'a [u8]),
+    FnArray(&'a [u64]),
+    SymbolTable(&'a [SymbolTableEntry]),
+    DynSymbolTable(&'a [SymbolTableEntry]),
+    SymTabShIndex(&'a [u32]),
+    Rela(&'a [Rela]),
+    Rel(&'a [Rel]),
+    // Dynamic(&'a [Dynamic]),
+}
+
+#[derive(Debug)]
+#[repr(C)]
+pub struct Rel {
+    offset: u64,
+    info: u64,
+}
+#[derive(Debug)]
+#[repr(C)]
+pub struct Rela {
+    offset: u64,
+    info: u64,
+    addend: u64,
+}
+
+impl Rela {
+    pub fn get_offset(&self) -> u64 {
+        self.offset
+    }
+
+    pub fn get_addend(&self) -> u64 {
+        self.addend
+    }
+
+    pub fn get_symbol_table_index(&self) -> u32 {
+        (self.info >> 32) as u32
+    }
+
+    pub fn get_type(&self) -> u32 {
+        (self.info & 0xffffffff) as u32
+    }
+}
+
+#[derive(Debug)]
+#[repr(C)]
+pub struct SymbolTableEntry {
+    name: u32,
+    info: u8,
+    other: u8,
+    shndx: u16,
+    value: u64,
+    size: u64,
+}
+
+impl SymbolTableEntry {
+    pub fn get_name<'a>(&'a self, file: &ElfFile<'a>) -> Result<&'a str, &'static str> {
+        file.get_string(self.name)
+    }
+
+    pub fn get_section_header<'a>(
+        &'a self,
+        file: &ElfFile<'a>,
+        self_index: usize,
+    ) -> Result<&'a SectionHeader, &'static str> {
+        match self.shndx {
+            SHN_XINDEX => {
+                let header = file.find_section_by_name(".symtab_shndx");
+                if let Some(header) = header {
+                    assert_eq!(header.get_type()?, SectionHeaderType::SymTabShIndex);
+                    if let SectionData::SymTabShIndex(data) = header.get_data(file)? {
+                        let index = data[self_index] as u16;
+                        assert_ne!(index, SHN_UNDEF);
+                        file.get_section_header(index)
+                    } else {
+                        Err("expected SymTabShIndex")
+                    }
+                } else {
+                    Err("no `.symtab_shndx` section")
+                }
+            }
+            SHN_UNDEF | SHN_ABS | SHN_COMMON => Err("reserved section header index"),
+            i => file.get_section_header(i),
+        }
+    }
+
+    #[inline]
+    pub const fn name(&self) -> u32 {
+        self.name
+    }
+
+    #[inline]
+    pub const fn size(&self) -> u64 {
+        self.size
+    }
+
+    #[inline]
+    pub const fn value(&self) -> u64 {
+        self.value
+    }
+
+    #[inline]
+    pub const fn info(&self) -> u8 {
+        self.info
+    }
+
+    #[inline]
+    pub const fn shndx(&self) -> u16 {
+        self.shndx
+    }
+}
 
 
 
@@ -255,6 +590,9 @@ unsafe impl Pod for i128 {}
 unsafe impl Pod for HeaderIdent {}
 unsafe impl Pod for HeaderBody {}
 unsafe impl Pod for SectionHeader {}
+unsafe impl Pod for Rel {}
+unsafe impl Pod for Rela {}
+unsafe impl Pod for SymbolTableEntry {}
 
 unsafe fn pod_read<T: Pod>(bytes: &[u8]) -> &T {
     assert!(size_of::<T>() <= bytes.len());
@@ -275,4 +613,33 @@ fn read_until_null(input: &[u8]) -> &[u8] {
     }
 
     panic!("No null byte in input");
+}
+
+fn read_str(input: &[u8]) -> &str {
+    std::str::from_utf8(read_str_bytes(input)).expect("invalid UTF-8 string")
+}
+
+fn read_str_bytes(input: &[u8]) -> &[u8] {
+    for (i, byte) in input.iter().enumerate() {
+        if *byte == 0 {
+            return &input[..i];
+        }
+    }
+
+    panic!("no null byte in input");
+}
+
+fn read_array<T: Pod>(input: &[u8]) -> &[T] {
+    let t_size = size_of::<T>();
+    assert!(t_size > 0, "Can't read arrays of zero-sized types");
+    assert!(input.len() % t_size == 0);
+    let addr = input.as_ptr() as usize;
+    assert!(addr & (align_of::<T>() - 1) == 0);
+
+    unsafe { read_array_unsafe(input) }
+}
+
+unsafe fn read_array_unsafe<T: Sized>(input: &[u8]) -> &[T] {
+    let ptr = input.as_ptr() as *const T;
+    unsafe { std::slice::from_raw_parts(ptr, input.len() / size_of::<T>()) }
 }
