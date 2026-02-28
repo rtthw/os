9f77c811848b3d3deed33f512fb85acb8427622e
rtthw <172864013+rtthw@users.noreply.github.com>
2026-02-28 07:18:25 -0500

    Add support for parsing PCI BARs

--- kernel/src/pci.rs
+++ kernel/src/pci.rs
@@ -16,7 +16,6 @@ pub struct Device {
     pub vendor_id: u16,
     pub class: u16,
     pub header_type: u8,
-    pub bars: [u32; 6],
     pub interrupt_line: u8,
     pub interrupt_pin: u8,
 }
@@ -52,6 +51,88 @@ impl Device {
     pub fn capabilities(&self) -> Vec<Capability> {
         get_capabilities(self.bus, self.device, 0)
     }
+
+    pub fn bar(&self, slot: u8) -> Option<Bar> {
+        if slot >= 6 {
+            return None;
+        }
+
+        let offset = 16 + slot * 4;
+        let bar = unsafe { read(self.bus, self.device, self.function, offset) };
+
+        if !u32_get_bit(bar, 0) {
+            let prefetchable = u32_get_bit(bar, 3);
+            let address = u32_bit_range(bar, 4, 32) << 4;
+
+            match u32_bit_range(bar, 1, 3) {
+                0b00 => {
+                    let size = unsafe {
+                        write(self.bus, self.device, self.function, offset, 0xffffffff);
+                        let readback = read(self.bus, self.device, self.function, offset);
+                        write(self.bus, self.device, self.function, offset, address);
+
+                        // BAR is unimplemented.
+                        if readback == 0 {
+                            return None;
+                        }
+
+                        1 << u32_set_range(readback, 0, 4, 0).trailing_zeros()
+                    };
+
+                    Some(Bar::Mem32 {
+                        address,
+                        size,
+                        prefetchable,
+                    })
+                }
+                0b10 => {
+                    // If we are looking at the last slot, then we can't read a 64-bit value.
+                    if slot >= 5 {
+                        return None;
+                    }
+
+                    let address_upper =
+                        unsafe { read(self.bus, self.device, self.function, offset + 4) };
+
+                    let size = unsafe {
+                        write(self.bus, self.device, self.function, offset, 0xFFFFFFFF);
+                        write(self.bus, self.device, self.function, offset + 4, 0xFFFFFFFF);
+                        let mut readback_low = read(self.bus, self.device, self.function, offset);
+                        let readback_high = read(self.bus, self.device, self.function, offset + 4);
+                        write(self.bus, self.device, self.function, offset, address);
+                        write(
+                            self.bus,
+                            self.device,
+                            self.function,
+                            offset + 4,
+                            address_upper,
+                        );
+
+                        readback_low = u32_set_range(readback_low, 0, 4, 0);
+                        if readback_low != 0 {
+                            (1 << readback_low.trailing_zeros()) as u64
+                        } else {
+                            1 << ((readback_high.trailing_zeros() + 32) as u64)
+                        }
+                    };
+
+                    let address = u64_set_range(address as u64, 32, 64, address_upper as u64);
+
+                    Some(Bar::Mem64 {
+                        address,
+                        size,
+                        prefetchable,
+                    })
+                }
+
+                _ => panic!("unknown PCI BAR memory type"),
+            }
+        } else {
+            Some(Bar::Io {
+                port: u32_bit_range(bar, 2, 32) << 2,
+            })
+        }
+    }
 }
 
 impl Debug for Device {
@@ -70,7 +151,10 @@ impl Debug for Device {
             .field("function", &self.function)
             .field("class", &self.class)
             .field("header_type", &self.header_type)
-            .field("bars", &self.bars)
+            .field(
+                "bars",
+                &(0..6).map(|slot| self.bar(slot)).collect::<Vec<_>>(),
+            )
             .field("interrupt_line", &self.interrupt_line)
             .field("interrupt_pin", &self.interrupt_pin)
             .field("capabilities", &self.capabilities())
@@ -88,6 +172,23 @@ impl Debug for Capability {
     fn fmt(&self, f: &mut core::fmt::Formatter<'_>) -> core::fmt::Result {
         f.write_fmt(format_args!("{} @ {:#x}", self.id, self.offset))
     }
+}
+
+#[derive(Clone, Debug)]
+pub enum Bar {
+    Mem32 {
+        address: u32,
+        size: u32,
+        prefetchable: bool,
+    },
+    Mem64 {
+        address: u64,
+        size: u64,
+        prefetchable: bool,
+    },
+    Io {
+        port: u32,
+    },
 }
 
 pub fn enumerate_devices() -> Vec<Device> {
@@ -118,16 +219,6 @@ fn get_device(bus: u8, device: u8) -> Option<Device> {
     let header_type = unsafe { read(bus, device, function, 0x0C) };
     let header_type = ((header_type >> 16) & 0xFF) as u8;
 
-    let mut bars = [0, 0, 0, 0, 0, 0];
-    unsafe {
-        bars[0] = read(bus, device, function, 0x10);
-        bars[1] = read(bus, device, function, 0x14);
-        bars[2] = read(bus, device, function, 0x18);
-        bars[3] = read(bus, device, function, 0x1C);
-        bars[4] = read(bus, device, function, 0x20);
-        bars[5] = read(bus, device, function, 0x24);
-    }
-
     let last_row = unsafe { read(bus, device, 0, 0x3C) };
 
     Some(Device {
@@ -138,7 +229,6 @@ fn get_device(bus: u8, device: u8) -> Option<Device> {
         vendor_id,
         class,
         header_type,
-        bars,
         interrupt_line: (last_row & 0xFF) as u8,
         interrupt_pin: ((last_row >> 8) & 0xFF) as u8,
     })
@@ -156,6 +246,21 @@ unsafe fn read(bus: u8, device: u8, function: u8, offset: u8) -> u32 {
     unsafe {
         Port::<u32>::new(0xCF8).write(address);
         Port::<u32>::new(0xCFC).read()
+    }
+}
+
+unsafe fn write(bus: u8, device: u8, function: u8, offset: u8, value: u32) {
+    let bus = bus as u32;
+    let device = device as u32;
+    let function = function as u32;
+    let offset = offset as u32;
+
+    let address =
+        ((bus << 16) | (device << 11) | (function << 8) | (offset & 0xfc) | 0x80000000) as u32;
+
+    unsafe {
+        Port::<u32>::new(0xCF8).write(address);
+        Port::<u32>::new(0xCFC).write(value);
     }
 }
 
@@ -191,6 +296,11 @@ const fn u32_bit_range(word: u32, start: usize, end: usize) -> u32 {
     bits >> start
 }
 
+const fn u32_get_bit(word: u32, bit: usize) -> bool {
+    assert!(bit < 32);
+    (word & (1 << bit)) != 0
+}
+
 const fn u32_set_bit(word: &mut u32, bit: usize, value: bool) -> &mut u32 {
     assert!(bit < 32);
 
@@ -201,4 +311,22 @@ const fn u32_set_bit(word: &mut u32, bit: usize, value: bool) -> &mut u32 {
     }
 
     word
+}
+
+const fn u32_set_range(num: u32, start: usize, end: usize, value: u32) -> u32 {
+    if start != end {
+        let bitmask: u32 = !(!0 << (32 - end) >> (32 - end) >> start << start);
+        (num & bitmask) | (value << start)
+    } else {
+        num
+    }
+}
+
+const fn u64_set_range(num: u64, start: usize, end: usize, value: u64) -> u64 {
+    if start != end {
+        let bitmask: u64 = !(!0 << (64 - end) >> (64 - end) >> start << start);
+        (num & bitmask) | (value << start)
+    } else {
+        num
+    }
 }
