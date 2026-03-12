72a0e84b4af3c45d965844e9ec32e74b43e74a0b
rtthw <172864013+rtthw@users.noreply.github.com>
2026-03-12 07:24:54 -0400

    Add the core fault handlers to the IDT

--- kernel-next/src/idt.rs
+++ kernel-next/src/idt.rs
@@ -3,7 +3,10 @@
 use {
     crate::gdt,
     log::info,
-    x86_64::structures::idt::{InterruptDescriptorTable, InterruptStackFrame},
+    x86_64::{
+        registers::control::Cr2,
+        structures::idt::{InterruptDescriptorTable, InterruptStackFrame, PageFaultErrorCode},
+    },
 };
 
 
@@ -11,13 +14,19 @@ use {
 static mut IDT: InterruptDescriptorTable = InterruptDescriptorTable::new();
 
 pub fn init() {
-    info!("Initializing GDT...");
+    info!("Initializing IDT...");
 
     #[allow(static_mut_refs)]
     unsafe {
         IDT.double_fault
             .set_handler_fn(double_fault_handler)
             .set_stack_index(gdt::DOUBLE_FAULT_IST);
+        IDT.page_fault
+            .set_handler_fn(page_fault_handler)
+            .set_stack_index(gdt::PAGE_FAULT_IST);
+        IDT.general_protection_fault
+            .set_handler_fn(general_protection_fault_handler)
+            .set_stack_index(gdt::GENERAL_PROTECTION_FAULT_IST);
 
         IDT.load();
     }
@@ -25,7 +34,22 @@ pub fn init() {
 
 extern "x86-interrupt" fn double_fault_handler(
     stack_frame: InterruptStackFrame,
-    _error_code: u64,
+    error_code: u64,
 ) -> ! {
-    panic!("DOUBLE_FAULT : {:#?}", stack_frame);
+    panic!("DOUBLE_FAULT({error_code}) : {stack_frame:#?}");
+}
+
+extern "x86-interrupt" fn page_fault_handler(
+    stack_frame: InterruptStackFrame,
+    error_code: PageFaultErrorCode,
+) {
+    let addr = Cr2::read_raw();
+    panic!("PAGE_FAULT({error_code:?}) @ {addr:#x} : {stack_frame:#?}");
+}
+
+extern "x86-interrupt" fn general_protection_fault_handler(
+    stack_frame: InterruptStackFrame,
+    error_code: u64,
+) {
+    panic!("GENERAL_PROTECTION_FAULT({error_code}) : {stack_frame:#?}");
 }
