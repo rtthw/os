1abc8bd84cbfd5fa937682f4f1b6d93ddad7d098
rtthw <172864013+rtthw@users.noreply.github.com>
2026-03-17 08:20:00 -0400

    Replace `uart_16550` dependency with an internal serial port driver

--- kernel-next/src/serial.rs
+++ kernel-next/src/serial.rs
@@ -1,6 +1,11 @@
 //! # Serial Port
 
-use {log::LogLevel, spin_mutex::Mutex, uart_16550::SerialPort};
+use {
+    core::fmt::Write,
+    log::LogLevel,
+    spin_mutex::Mutex,
+    x86_64::instructions::{interrupts::without_interrupts, port::Port},
+};
 
 
 pub static SERIAL1: Mutex<SerialPort> = Mutex::new(unsafe { SerialPort::new(0x3F8) });
@@ -12,8 +17,6 @@ pub fn init() {
 
 #[doc(hidden)]
 pub fn _print(args: core::fmt::Arguments) {
-    use {core::fmt::Write, x86_64::instructions::interrupts::without_interrupts};
-
     without_interrupts(|| {
         SERIAL1
             .lock()
@@ -85,5 +88,127 @@ impl log::Log for SerialLogger {
             },
             args,
         );
+    }
+}
+
+
+
+pub struct SerialPort(u16); // Must be public for macro.
+
+const INTERRUPT_ENABLE_REGISTER: u16 = 1;
+const FIFO_CONTROL_REGISTER: u16 = 2;
+const LINE_CONTROL_REGISTER: u16 = 3;
+const MODEM_CONTROL_REGISTER: u16 = 4;
+const LINE_STATUS_REGISTER: u16 = 5;
+const _MODEM_STATUS_REGISTER: u16 = 6;
+
+const LINE_STATUS_OUTPUT_EMPTY: u8 = 1 << 5;
+
+impl SerialPort {
+    pub const unsafe fn new(port: u16) -> Self {
+        Self(port)
+    }
+
+    // https://wiki.osdev.org/Serial_Ports#Programming_the_Serial_Communications_Port
+    pub fn init(&mut self) {
+        const DLAB: u8 = 1 << 7;
+        const WORD_LEN_8BIT: u8 = 0b_0000_0011;
+        const DIVISOR_LSB: u8 = 3;
+        const DIVISOR_MSB: u8 = 0;
+        const FIFO_ENABLE: u8 = 0b_0000_0001;
+        const FIFO_CLEAR_SEND: u8 = 0b_0000_0100;
+        const FIFO_CLEAR_RECV: u8 = 0b_0000_0010;
+        const FIFO_INT_LEVEL_14: u8 = 0b_1100_0000;
+        const DATA_TERMINAL_READY: u8 = 0b_0000_0001;
+        const REQUEST_TO_SEND: u8 = 0b_0000_0010;
+        const PIN_OUT2: u8 = 0b_0000_1000;
+
+        unsafe {
+            let mut data_port = Port::<u8>::new(self.0);
+            let mut interrupt_enable = Port::<u8>::new(self.0 + INTERRUPT_ENABLE_REGISTER);
+            let mut fifo_control = Port::<u8>::new(self.0 + FIFO_CONTROL_REGISTER);
+            let mut line_control = Port::<u8>::new(self.0 + LINE_CONTROL_REGISTER);
+            let mut modem_control = Port::<u8>::new(self.0 + MODEM_CONTROL_REGISTER);
+
+            // Disable interrupts.
+            interrupt_enable.write(0);
+
+            // Set the divisor latch access bit (DLAB).
+            line_control.write(DLAB);
+
+            // Set the baud rate. See the OSDev article for more:
+            //      https://wiki.osdev.org/Serial_Ports#Baud_Rate
+            data_port.write(DIVISOR_LSB);
+            interrupt_enable.write(DIVISOR_MSB);
+
+            // Finish setting the baud rate by clearing the DLAB, and at the same time set
+            // the word length to 8 bits. I know `& !DLAB` isn't doing anything, it's easier
+            // to read this way.
+            line_control.write(WORD_LEN_8BIT & !DLAB);
+
+            // Enable FIFO, clear it, and set the interrupt trigger level to 14 bytes.
+            fifo_control.write(FIFO_ENABLE | FIFO_CLEAR_SEND | FIFO_CLEAR_RECV | FIFO_INT_LEVEL_14);
+
+            // Set the data terminal ready pin, signal request to send, and enable hardware
+            // pin OUT2 (enable IRQ).
+            modem_control.write(DATA_TERMINAL_READY | REQUEST_TO_SEND | PIN_OUT2);
+
+            // Enable interrupts.
+            interrupt_enable.write(1);
+        }
+    }
+
+    pub fn write(&mut self, byte: u8) {
+        match byte {
+            0x08 /* BS */ | 0x7F /* DEL */ => {
+                self.send(0x08); // Move back 1 character.
+                self.send(b' '); // Write a space (also moves forward 1 character).
+                self.send(0x08); // Go back to before the space.
+            }
+            b'\n' => {
+                self.send(b'\r');
+                self.send(b'\n');
+            }
+
+            other => {
+                self.send(other);
+            }
+        }
+    }
+
+    pub fn send(&mut self, byte: u8) {
+        while !self.try_send(byte) {
+            core::hint::spin_loop();
+        }
+    }
+
+    pub fn try_send(&mut self, byte: u8) -> bool {
+        if self.line_output_empty() {
+            unsafe {
+                Port::<u8>::new(self.0).write(byte);
+            }
+
+            true
+        } else {
+            false
+        }
+    }
+
+    pub fn read_line_status(&self) -> u8 {
+        unsafe { Port::<u8>::new(self.0 + LINE_STATUS_REGISTER).read() }
+    }
+
+    pub fn line_output_empty(&self) -> bool {
+        self.read_line_status() & LINE_STATUS_OUTPUT_EMPTY == LINE_STATUS_OUTPUT_EMPTY
+    }
+}
+
+impl Write for SerialPort {
+    fn write_str(&mut self, s: &str) -> core::fmt::Result {
+        for byte in s.bytes() {
+            self.write(byte);
+        }
+
+        Ok(())
     }
 }
