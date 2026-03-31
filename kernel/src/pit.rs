eb90025da2f7c290a8c96370b78a0c5de5698c1c
rtthw <172864013+rtthw@users.noreply.github.com>
2026-03-31 09:38:13 -0400

    Use the x86 port crate within the kernel's PIT module

--- kernel/src/pit.rs
+++ kernel/src/pit.rs
@@ -1,7 +1,5 @@
 //! # Programmable Interval Timer (PIT)
 
-use x86_64::instructions::port::Port;
-
 
 const PIT_CHANNEL_2_PORT: u16 = 0x42;
 const PIT_COMMAND_REGISTER_PORT: u16 = 0x43;
@@ -16,35 +14,51 @@ pub fn sleep(microseconds: u16) {
     let frequency = PIT_DEFAULT_FREQUENCY_HZ / (1_000_000 / microseconds as u32);
 
     unsafe {
-        let mut pit_command = Port::<u8>::new(PIT_COMMAND_REGISTER_PORT);
-        let mut pit_channel_2 = Port::<u8>::new(PIT_CHANNEL_2_PORT);
-        let mut gate = Port::<u8>::new(PIT_CHANNEL_2_GATE_PORT);
-
         // Set the speaker channel 2 to be controlled by the PIT, with the following
         // config:
         //      | 10       | Select channel 2
         //      |   11     | Access mode: lobyte/hibyte
         //      |     001  | Operating mode: hardware re-triggerable one-shot
         //      |        0 | Four-digit BCD mode
-        let prev = gate.read();
-        gate.write(prev & 0b_11111101 | 0b_00000001);
-        pit_command.write(0b_10110010);
+        let prev = read_gate();
+        write_gate(prev & 0b_11111101 | 0b_00000001);
+        write_command(0b_10110010);
 
         // Set the frequency. We read from PS/2 port 0x60 between writing the low and
         // high bytes to act as a kind of delay/acknowledgement.
-        pit_channel_2.write(frequency as u8);
-        _ = Port::<u8>::new(0x60).read(); // ACK
-        pit_channel_2.write((frequency >> 8) as u8);
+        write_channel_2(frequency as u8);
+        _ = x86_port::read_u8(0x60); // ACK
+        write_channel_2((frequency >> 8) as u8);
 
-        // Reset the one-shot counter by clear bit 0, then setting it again.
-        let prev = gate.read() & 0b_11111110;
-        gate.write(prev);
-        gate.write(prev | 0b_00000001);
+        // Reset the one-shot counter by clearing bit 0, then setting it again.
+        let prev = read_gate() & 0b_11111110;
+        write_gate(prev);
+        write_gate(prev | 0b_00000001);
 
         // Finally, we can wait for the timer to finish, which happens when bit 5 is
         // cleared (the speaker moves "in").
-        while gate.read() & 0b_00100000 != 0 {
+        while read_gate() & 0b_00100000 != 0 {
             core::hint::spin_loop();
         }
     }
+}
+
+#[inline(always)]
+unsafe fn read_gate() -> u8 {
+    unsafe { x86_port::read_u8(PIT_CHANNEL_2_GATE_PORT) }
+}
+
+#[inline(always)]
+unsafe fn write_gate(value: u8) {
+    unsafe { x86_port::write_u8(PIT_CHANNEL_2_GATE_PORT, value) }
+}
+
+#[inline(always)]
+unsafe fn write_channel_2(value: u8) {
+    unsafe { x86_port::write_u8(PIT_CHANNEL_2_PORT, value) }
+}
+
+#[inline(always)]
+unsafe fn write_command(value: u8) {
+    unsafe { x86_port::write_u8(PIT_COMMAND_REGISTER_PORT, value) }
 }
