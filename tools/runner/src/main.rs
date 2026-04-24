1606fb6c5f395897eddf0a7931c8d087136f3ff5
rtthw <172864013+rtthw@users.noreply.github.com>
2026-04-24 08:51:06 -0400

    Create the runner tool to replace the run script

---
+++ tools/runner/src/main.rs
@@ -0,0 +1,197 @@
+//! # Runner
+
+use {
+    anyhow::{Result, bail},
+    std::{
+        env::{current_dir, set_current_dir},
+        fs::create_dir_all,
+        path::{Path, PathBuf},
+        process::{Command, ExitStatus, Stdio},
+    },
+};
+
+const OBJECT_SOURCES: &[(&str, &str)] = &[
+    ("crates", "boot-info"),
+    ("crates", "framebuffer"),
+    ("crates", "input"),
+    ("crates", "time"),
+    ("drivers", "pit"),
+    ("", "example"),
+    ("example", "example-dep"),
+];
+
+fn main() -> Result<()> {
+    let workspace_dir = find_workspace_dir()?;
+    let _ = set_current_dir(&workspace_dir);
+
+    println!("Creating build directories...");
+
+    let kernel_dir = workspace_dir.join("kernel");
+    if !kernel_dir.exists() {
+        bail!("`./kernel` not found");
+    }
+    let boot_dir = kernel_dir.join("esp/efi/boot");
+    if !boot_dir.exists() {
+        create_dir_all(&boot_dir)?;
+    }
+    let uefi_dir = kernel_dir.join("firmware/uefi");
+    if !uefi_dir.exists() {
+        create_dir_all(&uefi_dir)?;
+        std::fs::copy(
+            "/usr/share/OVMF/OVMF_CODE.fd",
+            workspace_dir.join("kernel/firmware/uefi/OVMF_CODE.fd"),
+        )?;
+        std::fs::copy(
+            "/usr/share/OVMF/OVMF_VARS.fd",
+            workspace_dir.join("kernel/firmware/uefi/OVMF_VARS.fd"),
+        )?;
+    }
+
+    if !is_program_installed("rustc")? {
+        bail!("Rust is not installed");
+    }
+
+    println!("Building bootloader...");
+
+    set_current_dir(workspace_dir.join("bootloader"))?;
+    let cargo_exit_status = Command::new("cargo")
+        .arg("build")
+        .arg("--release")
+        .status()?;
+    if !cargo_exit_status.success() {
+        bail!(
+            "Failed to build bootloader, cargo exited with code: {}",
+            cargo_exit_status.code().unwrap(),
+        );
+    }
+    set_current_dir(&workspace_dir)?;
+    std::fs::copy(
+        workspace_dir.join("target/x86_64-unknown-uefi/release/bootloader.efi"),
+        boot_dir.join("bootx64.efi"),
+    )?;
+
+    for (crate_dir, crate_name) in OBJECT_SOURCES {
+        println!("\tBuilding `{crate_name}`...");
+        let cargo_exit_status = build_crate_object(&workspace_dir, crate_dir, crate_name)?;
+        if !cargo_exit_status.success() {
+            bail!(
+                "Failed to build `{crate_name}`, cargo exited with code: {}",
+                cargo_exit_status.code().unwrap(),
+            );
+        }
+    }
+
+    println!("Building kernel...");
+
+    let cargo_exit_status = Command::new("cargo")
+        .arg("rustc")
+        .arg("--release")
+        .arg("--manifest-path=kernel/Cargo.toml")
+        .arg("--target=kernel/x86_64-app.json")
+        .arg("-Zbuild-std=core,alloc")
+        .arg("-Zbuild-std-features=compiler-builtins-mem")
+        .arg("--")
+        .arg("-Clink-arg=-T")
+        .arg("-Clink-arg=kernel/kernel_x86_64.ld")
+        .arg("-Clink-arg=-z")
+        .arg("-Clink-arg=max-page-size=0x1000")
+        .arg("--emit=link=kernel/esp/kernel")
+        .status()?;
+    if !cargo_exit_status.success() {
+        bail!(
+            "Failed to build kernel, cargo exited with code: {}",
+            cargo_exit_status.code().unwrap(),
+        );
+    }
+
+    let qemu_exit_status = run_qemu()?;
+    if !qemu_exit_status.success() {
+        bail!(
+            "QEMU failed with exit code {}",
+            qemu_exit_status.code().unwrap(),
+        );
+    }
+
+    Ok(())
+}
+
+fn find_workspace_dir() -> Result<PathBuf> {
+    let current_dir = current_dir()?.canonicalize()?;
+    for path in current_dir.ancestors() {
+        if path.is_dir() && path.file_name().is_some_and(|name| name == "rhubarb") {
+            return Ok(path.to_path_buf());
+        }
+    }
+
+    bail!("failed to find the workspace directory")
+}
+
+fn build_crate_object(workspace_dir: &Path, dir: &str, name: &str) -> Result<ExitStatus> {
+    // let parent_dir = if dir.is_empty() {}
+    let manifest_path = workspace_dir.join(dir).join(name).join("Cargo.toml");
+    let object_path = workspace_dir
+        .join("kernel/esp")
+        .join(format!("{}.o", crate_name_to_object_name(name)));
+
+    Ok(Command::new("cargo")
+        .arg("rustc")
+        .arg("--release")
+        .arg(format!("--manifest-path={}", manifest_path.display()))
+        .arg("--target=kernel/x86_64-app.json")
+        .arg("-Zbuild-std=core,alloc")
+        .arg("-Zbuild-std-features=compiler-builtins-mem")
+        .arg("--")
+        .arg("--crate-type=lib")
+        .arg(format!("--emit=obj={}", object_path.display()))
+        .arg("-Clink-dead-code=yes")
+        .arg("-Zshare-generics=no")
+        .status()?)
+}
+
+fn crate_name_to_object_name(crate_name: &str) -> String {
+    crate_name.replace('-', "_")
+}
+
+fn is_program_installed(name: &str) -> Result<bool> {
+    Ok(Command::new("which")
+        .arg(name)
+        .stdout(Stdio::null())
+        .status()
+        .map(|status| status.success())?)
+}
+
+fn run_qemu() -> Result<ExitStatus> {
+    let qemu_program = "qemu-system-x86_64";
+    if !is_program_installed(qemu_program)? {
+        bail!("QEMU is not installed");
+    }
+
+    Ok(Command::new(qemu_program)
+        .args([
+            "-accel",
+            "kvm",
+            "-m",
+            "256M",
+            "-smp",
+            "4",
+            "-rtc",
+            "base=localtime",
+            "-display",
+            "gtk,show-tabs=on",
+            "-device",
+            "virtio-keyboard",
+            "-device",
+            "virtio-mouse",
+            "-vga",
+            "virtio",
+            "-serial",
+            "stdio",
+            "-drive",
+            "format=raw,file=fat:rw:kernel/esp",
+            "-drive",
+            "if=pflash,format=raw,readonly=on,file=kernel/firmware/uefi/OVMF_CODE.fd",
+            "-drive",
+            "if=pflash,format=raw,readonly=on,file=kernel/firmware/uefi/OVMF_VARS.fd",
+        ])
+        .status()?)
+}
